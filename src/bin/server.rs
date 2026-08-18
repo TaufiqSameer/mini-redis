c915dbcde8bc119d1fda7741277274dbb417c74a
NemesisX-ai <sameertanker@gmail.com>
2026-08-18 23:07:07 +0530

    connection implementation

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,135 @@
-use bytes::Bytes;
-use mini_redis::{Connection, Frame};
+use bytes::{Buf, Bytes, BytesMut};
+use mini_redis::{Frame, Result};
 use std::collections::HashMap;
+use std::io::{self, Cursor};
 use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpSocket, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
+use tokio::net::{TcpListener, TcpStream};
+
+// use crate::frame::{self, Frame};
+
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+struct Connection {
+    stream: BufWriter<TcpStream>,
+    buffer : BytesMut
+
+}
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Connection {
+        Connection {
+            stream: BufWriter::new(socket),
+            buffer: BytesMut::with_capacity(4 * 1024),
+        }
+    }
+    pub async fn read_frame(&mut self)-> Result<Option<Frame>>{
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
+    }
+    fn parse_frame(&mut self) -> crate::Result<Option<Frame>> {
+
+        let mut buf = Cursor::new(&self.buffer[..]);
+
+        match Frame::check(&mut buf) {
+            Ok(_) => {
+       
+                let len = buf.position() as usize;
+
+                buf.set_position(0);
+
+
+                let frame = Frame::parse(&mut buf)?;
+                self.buffer.advance(len);
+
+                Ok(Some(frame))
+            }
+      
+            Err(mini_redis::frame::Error::Incomplete) => Ok(None),
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    pub async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
+    
+        match frame {
+            Frame::Array(val) => {
+                self.stream.write_u8(b'*').await?;
+
+                self.write_decimal(val.len() as u64).await?;
+
+                for entry in &**val {
+                    self.write_value(entry).await?;
+                }
+            }
+        
+            _ => self.write_value(frame).await?,
+        }
+
+        self.stream.flush().await
+    }
+
+    async fn write_value(&mut self, frame: &Frame) -> io::Result<()> {
+        match frame {
+            Frame::Simple(val) => {
+                self.stream.write_u8(b'+').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Error(val) => {
+                self.stream.write_u8(b'-').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Integer(val) => {
+                self.stream.write_u8(b':').await?;
+                self.write_decimal(*val).await?;
+            }
+            Frame::Null => {
+                self.stream.write_all(b"$-1\r\n").await?;
+            }
+            Frame::Bulk(val) => {
+                let len = val.len();
+
+                self.stream.write_u8(b'$').await?;
+                self.write_decimal(len as u64).await?;
+                self.stream.write_all(val).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+        
+            Frame::Array(_val) => unreachable!(),
+        }
+
+        Ok(())
+    }
+
+
+    async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+        use std::io::Write;
+
+        let mut buf = [0u8; 12];
+        let mut buf = Cursor::new(&mut buf[..]);
+        write!(&mut buf, "{}", val)?;
+
+        let pos = buf.position() as usize;
+        self.stream.write_all(&buf.get_ref()[..pos]).await?;
+        self.stream.write_all(b"\r\n").await?;
+
+        Ok(())
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
